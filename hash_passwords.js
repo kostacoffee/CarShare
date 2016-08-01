faabd7d1c1f8c53cc091eb4c97cf4e3ac5b788a1
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2016-08-01 13:42:30 +0100

    Update hash_passwords.js
    the right way to do it, using `tx`, `each` and `batch`.

--- hash_passwords.js
+++ hash_passwords.js
@@ -8,21 +8,17 @@ var db = pgp({
 	password : 'MnU79g&@s9nacLcB'
 });
 
-db.many("SELECT memberno, nickname, password FROM member").then(function(data){
-	for (var i = 0; i < data.length; i++){
-		var pw = data[i].password.trim();
-		var memNo = data[i].memberno;
-		var salt = crypto.randomBytes(16).toString('base64');
-		var hash = crypto.createHmac('sha512', salt).update(pw).digest('base64');
-		db.query("UPDATE member SET pw_salt=$1, password=$2 where memberno=$3", [salt, hash, memNo])
-		.then(function(){
-			console.log("ran query on member");
-		})
-		.catch(function(error){
-			console.log(error);
-		});
-	}
+db.tx(function (t) {
+    return t.map("SELECT memberno, nickname, password FROM member", [], update).then(t.batch);
+
+    function update(member) {
+        var pw = member.password.trim();
+        var memNo = member.memberno;
+        var salt = crypto.randomBytes(16).toString('base64');
+        var hash = crypto.createHmac('sha512', salt).update(pw).digest('base64');
+        return t.none("UPDATE member SET pw_salt=$1, password=$2 where memberno=$3", [salt, hash, memNo]);
+    }
 })
-.catch(function(error){
-	console.log(error);
-});
+    .catch(function (error) {
+        console.log(error);
+    });
